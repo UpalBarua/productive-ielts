6622b8766c82b0e5bd94cc0e70fb56bfb73d1208
UpalBarua <upalbarua52@gmail.com>
2024-09-07 10:18:09 +0600

    add: newsletter section

---
+++ client/src/components/newsletter.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { IconSend } from "@tabler/icons-react";
+
+export function Newsletter() {
+  return (
+    <section className="container py-20">
+      <div className="relative isolate grid items-start gap-2 overflow-hidden rounded-lg border bg-secondary p-8 sm:grid-cols-2 sm:gap-x-4">
+        <h2 className="text-pretty text-3xl font-extrabold tracking-tight sm:col-[1/2]">
+          Newsletter
+        </h2>
+        <p className="max-w-[40ch] pb-4 leading-relaxed text-secondary-foreground sm:col-[1/2]">
+          Keep pace with SecureCloud advancements! Join our mailing list for
+          selective, noteworthy updates.
+        </p>
+        <form className="flex h-12 w-full rounded-lg border border-input bg-background p-1 text-sm">
+          <label htmlFor="email-address" className="sr-only">
+            Email address
+          </label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            className="bg-transparent size-full px-2 outline-none"
+          />
+          <Button
+            type="submit"
+            className="aspect-square p-0 sm:aspect-[initial] sm:px-4"
+          >
+            <IconSend className="size-5 sm:size-4" />
+            <span className="hidden lg:block">Subscribe</span>
+          </Button>
+          <div className="absolute right-0 top-1/2 -z-10 size-[80rem] rounded-full bg-primary opacity-20 blur-2xl" />
+        </form>
+      </div>
+    </section>
+  );
+}
