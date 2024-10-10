fe1d67a2176336af31ee23fabb35060b4663a8b9
UpalBarua <upalbarua52@gmail.com>
2024-10-10 10:58:40 +0600

    refactor: app dir and layouts

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,21 +1,17 @@
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type RootLayoutProps = {
   children: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en" className={raleway.className}>
       <body className="grid min-h-screen bg-background text-foreground antialiased">
-        {/* <Navbar /> */}
         {children}
-        {/* <Footer /> */}
       </body>
     </html>
   );
