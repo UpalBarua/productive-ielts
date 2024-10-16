c3f18e2c225fbf01e2a9e868b1f425bff58f2d38
UpalBarua <upalbarua52@gmail.com>
2024-10-16 11:49:13 +0600

    add: quiz submitting functionality

--- client/src/components/quiz.tsx
+++ client/src/components/quiz.tsx
@@ -1,10 +1,26 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem } from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// TODO: do proper validation
+const answersSchema = z.object({
+  answers: z.array(
+    z.object({
+      answer: z.string(),
+    }),
+  ),
+});
+
+type AnswersSchema = z.infer<typeof answersSchema>;
 
 const quiz = [
   {
@@ -46,72 +62,106 @@ const quiz = [
 ] as const;
 
 export function Quiz() {
-  const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLFormElement | null>(null);
+
+  const form = useForm<AnswersSchema>({
+    resolver: zodResolver(answersSchema),
+    defaultValues: {
+      answers: quiz.map(() => ({ answer: "" })),
+    },
+  });
+
+  function onSubmit(values: AnswersSchema) {
+    console.log(values);
+    alert("Quiz Submitted ✅");
+  }
+
+  // TODO: improve this function
+  function scrollQuizQuestions(direction: "NEXT" | "PREVIOUS") {
+    const element = ref?.current;
+    const clientWidth = (element?.querySelector("div")?.clientWidth || 0) + 24;
+
+    element?.scrollBy({
+      left:
+        direction === "NEXT"
+          ? clientWidth
+          : direction === "PREVIOUS"
+            ? -clientWidth
+            : 0,
+      behavior: "smooth",
+    });
+  }
 
   return (
     <section className="rounded-xl bg-secondary p-2 shadow">
       <div className="p-4">
-        <div ref={ref} className="flex gap-x-6 overflow-hidden">
-          {quiz.map(({ question, options }, index) => (
-            <div key={index} className="min-w-full">
-              <p className="leadig-relaxed pb-6 text-xl font-medium">
-                {question}
-              </p>
-              <RadioGroup defaultValue="option-one">
-                {options.map((option, index) => (
-                  <div
-                    key={index}
-                    className="relative flex h-14 items-center rounded-lg border bg-background px-4"
-                  >
-                    <Label className="text-base" htmlFor={option}>
-                      <span className="pe-2 font-bold">
-                        {String.fromCharCode(index + 65)}.
-                      </span>
-                      {option}
-                    </Label>
-                    <RadioGroupItem
-                      className="absolute inset-0 h-full w-full rounded-lg border-primary bg-primary/15 [&>span]:hidden"
-                      value={option}
-                      id={option}
-                    />
-                  </div>
-                ))}
-              </RadioGroup>
-            </div>
-          ))}
-        </div>
+        <Form {...form}>
+          <form
+            id="quiz-form"
+            ref={ref}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex gap-x-6 overflow-hidden"
+          >
+            {quiz.map(({ question, options }, index) => (
+              <FormField
+                key={question + index}
+                control={form.control}
+                name={`answers.${index}.answer`}
+                render={({ field }) => (
+                  <FormItem key={index} className="min-w-full">
+                    <p className="leadig-relaxed pb-6 text-xl font-medium">
+                      {question}
+                    </p>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      {options.map((option, index) => (
+                        <div
+                          key={index}
+                          className="relative flex h-14 items-center rounded-lg border bg-background px-4"
+                        >
+                          <Label className="text-base" htmlFor={option}>
+                            <span className="pe-2 font-bold">
+                              {String.fromCharCode(index + 65)}.
+                            </span>
+                            {option}
+                          </Label>
+                          <RadioGroupItem
+                            className={cn(
+                              "absolute inset-0 h-full w-full rounded-lg border-border transition-colors duration-200 [&>span]:hidden",
+                              {
+                                "border-primary bg-primary/15":
+                                  option === field.value,
+                              },
+                            )}
+                            value={option}
+                            id={option}
+                          />
+                        </div>
+                      ))}
+                    </RadioGroup>
+                  </FormItem>
+                )}
+              />
+            ))}
+          </form>
+        </Form>
       </div>
-      <div className="flex items-center justify-end gap-4 px-2 pb-2 pt-5">
+      <div className="grid grid-cols-[max-content,1fr,max-content,max-content] items-center justify-end gap-4 px-2 pb-2 pt-5">
+        <Button type="submit" form="quiz-form">
+          Submit Answers
+        </Button>
         <Button
+          className="col-[3/4]"
           size="sm"
-          variant="secondary"
-          onClick={() => {
-            const element = ref?.current;
-            const clientWidth = -(
-              element?.querySelector("div")?.clientWidth + 24
-            );
-
-            element?.scrollBy({
-              left: clientWidth,
-              behavior: "smooth",
-            });
-          }}
+          variant="outline"
+          onClick={() => scrollQuizQuestions("PREVIOUS")}
         >
           <IconChevronLeft className="size-4" />
           <span>Previous</span>
         </Button>
-        <Button
-          size="sm"
-          onClick={() => {
-            const element = ref?.current;
-            const clientWidth = element?.querySelector("div")?.clientWidth + 24;
-
-            element?.scrollBy({
-              left: clientWidth,
-              behavior: "smooth",
-            });
-          }}
-        >
+        <Button size="sm" onClick={() => scrollQuizQuestions("NEXT")}>
           <span>Next</span>
           <IconChevronRight className="size-4" />
         </Button>
